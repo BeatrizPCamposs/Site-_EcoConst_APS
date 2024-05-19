cfb8c4a6ce13684fa8b5882d5133c244343d3530
Beatriz Campos <101807045+BeatrizPCamposs@users.noreply.github.com>
2024-05-19 10:47:33 -0300

    Update
    Adicionando páginas Home e Contato

---
+++ APS - SITE - ECOCONST/js/script.js
@@ -0,0 +1,5 @@
+/* Seção 1 - Botão de "Mais Informações" */
+function scroolSection(){
+    const section = document.getElementById('secao2');
+    section.scrollIntoView({ behavior: 'smooth' }); /* Defini a posição desejada da barra de rolagem */
+}
